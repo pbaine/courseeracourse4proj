0eedb00d4525a0c8731d271614c653a32b1975a9
Patrick Baine <Patrickbaine@gmail.com>
2017-04-12 22:15:02 -0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -6,7 +6,6 @@
 #Appropriately labels the data set with descriptive variable names.
 #From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 #Good luck!
-setwd('/Users/ECFC3/Documents/courseera')
 
 library(stringr)
 library(dplyr)
